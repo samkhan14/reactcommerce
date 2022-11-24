2fba16cb7973ccd14b021ec4e61ece1a44100d37
Sumaim Ahmed <sumaimahmed@gmail.com>
2022-11-25 00:28:03 +0500

    v3

--- src/App.js
+++ src/App.js
@@ -5,12 +5,15 @@ import Contact from './Pages/Contact/Contact';
 import Products from './Pages/Products/Products';
 import SingleProduct from './Pages/Products/SingleProduct';
 import Cart from './Pages/Cart/Cart';
+import { ErrorPage } from './Pages/ErrorPage/ErrorPage';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { GlobalStyleComponent } from './Components/GlobalStyleComponent';
 
 function App() {
   return (
     <>
     <Router>
+      <GlobalStyleComponent />
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/About" element={<About />} />
@@ -18,6 +21,7 @@ function App() {
         <Route path="/Contact" element={<Contact />} />
         <Route path="/SingleProduct/:id" element={<SingleProduct/>} />
         <Route path="/Cart" element={<Cart />} />
+        <Route path="*" element={<ErrorPage />} />
       </Routes>
     </Router>
     
